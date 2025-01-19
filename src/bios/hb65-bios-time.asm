4a93ce0ce1a6d3afde75f676bb8443fba505863d
Cole Campbell <cole.campbell@twistedlogik.net>
2025-01-19 16:30:38 -0500

    Preserve registers in TIME procedures

--- src/bios/hb65-bios-time.asm
+++ src/bios/hb65-bios-time.asm
@@ -8,17 +8,19 @@
 .SEGMENT "BIOS"
 
 ; TIME_DELAY_50US procedure
-; Modifies: X, flags
+; Modifies: n/a
 ;
 ; Delays for approximately 50 microseconds (assuming a 3.15 MHz clock).
 .PROC TIME_DELAY_50US
-                ; 6 cycles (JSR)
-    LDX #$1D	; 2 cycles
+              ; 6 cycles (JSR)
+    PHX       ; 3 cycles
+    LDX #$1C	; 2 cycles
   : DEX		    ; 2 cycles
     BNE :-		; 3 cycles / 2 cycles
+    PLX       ; 4 cycleds
     RTS		    ; 6 cycles
-                ; 14 cycles [overhead] + (5 * X) - 1 cycles [loop] = 158 cycles (X = $1D)
-                ; 158 cycles * 318 ns/cycle = 50,244 nanoseconds = 50.244 microseconds
+              ; 21 cycles [overhead] + (5 * X) - 1 cycles [loop] = 160 cycles (X = $1C)
+              ; 160 cycles * 318 ns/cycle = 50.88 microseconds
 .ENDPROC
 .EXPORT TIME_DELAY_50US
 
@@ -27,16 +29,20 @@
 ;
 ; Delays for approximately 1 millisecond (assuming a 3.15 MHz clock).
 .PROC TIME_DELAY_1MS
-                ; 6 cycles (JSR)
+              ; 6 cycles (JSR)
+    PHX       ; 3 cycles
+    PHY       ; 3 cycles
     LDY #$04	; 2 cycles
   : LDX #$9B	; 2 cycles
   : DEX		    ; 2 cycles
     BNE :-		; 3 cycles / 2 cycles
     DEY		    ; 2 cycles
     BNE :--		; 3 cycles / 2 cycles
+    PLY       ; 4 cycles
+    PLX       ; 4 cycles
     RTS		    ; 6 cycles
-                ; 14 cycles [overhead] + (Y * (7 + (2 + (5 * X) - 1))) - 1 cycles [loop] = 3145 cycles (X = $9B, Y = $04)
-                ; 3145 cycles * 318 ns/cycle = 1.00011 milliseconds
+              ; 28 cycles [overhead] + (Y * (7 + (2 + (5 * X) - 1))) - 1 cycles [loop] = 3159 cycles (X = $9B, Y = $04)
+              ; 3159 cycles * 318 ns/cycle = 1.004562 milliseconds
 .ENDPROC
 .EXPORT TIME_DELAY_1MS
 
@@ -45,15 +51,19 @@
 ;
 ; Delays for approximately 50 milliseconds (assuming a 3.15 MHz clock).
 .PROC TIME_DELAY_50MS
-                ; 6 cycles (JSR)
-    LDY #$C8	; 2 cycles
+              ; 6 cycles (JSR)
+    PHX       ; 3 cycles
+    PHY       ; 3 cycles
+    LDY #$C9	; 2 cycles
   : LDX #$9B	; 2 cycles
   : DEX		    ; 2 cycles
     BNE :-		; 3 cycles / 2 cycles
     DEY		    ; 2 cycles
     BNE :--		; 3 cycles / 2 cycles
+    PLY       ; 4 cycles
+    PLX       ; 4 cycles
     RTS		    ; 6 cycles
-                ; 14 cycles [overhead] + (Y * (7 + (2 + (5 * X) - 1))) - 1 cycles [loop] = 3145 cycles (X = $9B, Y = $04)
-                ; 3145 cycles * 318 ns/cycle = 1.00011 milliseconds
+              ; 28 cycles [overhead] + (Y * (7 + (2 + (5 * X) - 1))) - 1 cycles [loop] = 157410 cycles (X = $9B, Y = $C9)
+              ; 157410 cycles * 318 ns/cycle = 50.05638 milliseconds
 .ENDPROC
 .EXPORT TIME_DELAY_50MS
