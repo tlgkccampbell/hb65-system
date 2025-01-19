e1493e59b9902fb081be30fa0558fcffe3878a02
Cole Campbell <cole.campbell@twistedlogik.net>
2025-01-19 16:33:13 -0500

    UART cleanup

--- src/bios/hb65-bios-uart.asm
+++ src/bios/hb65-bios-uart.asm
@@ -8,52 +8,54 @@
 .SEGMENT "BIOS"
 
 ; UART_INIT procedure
-; Modifies: flags, A
+; Modifies: n/a
 ;
 ; Initializes the system's UARTs, if it has any.
 .PROC UART_INIT
     .IFDEF ACIA_REGISTERS
-        LDA     #$1E                ; 8-N-1, 9600 baud.
-        STA     ACIA_CTRL
-        LDA     #$0B                ; No parity, no echo, no interrupts.
-        STA     ACIA_CMD
+        PHA
+        LDA #$1E            ; 8-N-1, 9600 baud.
+        STA ACIA_CTRL
+        LDA #$0B            ; No parity, no echo, no interrupts.
+        STA ACIA_CMD
+        PLA
     .ENDIF
     RTS
 .ENDPROC
 .EXPORT UART_INIT
 
 ; UART_GETCH procedure
-; Modifies: flags, A
+; Modifies: A
 ;
 ; Attempts to read the next character from the system's first
 ; UART. If a character was read, the carry bit is set. Otherwise,
 ; the carry bit is cleared.
 .PROC UART_GETCH
     .IFDEF ACIA_REGISTERS
-        LDA     ACIA_STATUS
-        AND     #$08
-        BEQ     NOKEY
-        LDA     ACIA_DATA
+        LDA ACIA_STATUS
+        AND #$08
+        BEQ NOKEY
+        LDA ACIA_DATA
         SEC
         RTS
     .ENDIF
-NOKEY:
+  NOKEY:
     CLC
     RTS
 .ENDPROC
 .EXPORT UART_GETCH
 
 ; UART_PUTCH
-; Modifies: flags
+; Modifies: n/a
 ;
 ; Writes the value in A to the system's first UART.
 .PROC UART_PUTCH
-    PHA
     .IFDEF ACIA_REGISTERS
-    LOOP:
-        LDA     ACIA_STATUS
-        AND     #$10
-        BEQ     LOOP
+        PHA
+      LOOP:
+        LDA ACIA_STATUS
+        AND #$10
+        BEQ LOOP
         PLA
         STA ACIA_DATA
     .ENDIF
